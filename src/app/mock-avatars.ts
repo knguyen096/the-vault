acdfe47366e4c8f303730fa4a60c75b1df981248
Kelly Nguyen <88166463+knguyen096@users.noreply.github.com>
2021-08-09 16:32:47 -0600

    changes to avatar list
    Changed from normal list to a vertical material expansion panel

--- src/app/mock-avatars.ts
+++ src/app/mock-avatars.ts
@@ -1,10 +1,10 @@
 import { Avatar} from './avatar';
 
 export const AVATARS: Avatar[] = [
-    { id: 1, name: 'Hellfire' },
-    { id: 2, name: 'Vengeance' },
-    { id: 3, name: 'Hellhound' },
-    { id: 4, name: 'Banhmi' },
-    { id: 5, name: 'Candykaine' },
-    { id: 6, name: 'P-Model' }
+    { name: 'Hellfire', description: 'blue exorcist', price: 45 },
+    { name: 'Vengeance', description: 'anti valentines', price: 35 },
+    { name: 'Hellhound', description: 'dog avatar', price: 40 },
+    { name: 'Banhmi', description: 'vietnam', price: 35 },
+    { name: 'Candykaine', description: 'christmas', price: 35 },
+    { name: 'P-Model K', description: 'personal', price: 40 }
 ];
